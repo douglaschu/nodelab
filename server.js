238c95107e8e8a27a7974fd6949d38034928c87d
Doug Chu <douglas.l.chu@gmail.com>
2026-03-05 16:58:10 -0500

    chore: final cleanup commit

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
 "use strict";
+//import array
 const quotes = require('./arraymodule');
-console.log(quotes); //test if array exports right
+console.log(quotes); //test if array imports right
 
 let http = require('http');
 let server = (request, response) => {
     const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+        //formula for random array item
     response.writeHead(200, {"Content-type": "text"});
     response.write(randomQuote);
     response.end();
 }
+//creates local server on port 3000
 http.createServer(server).listen(3000);
 console.log('Server Started');
 
